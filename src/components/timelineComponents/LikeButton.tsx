02fb164d2d52925a2b167336830174b8818434dc
Elijah Gillit <egillit@gmail.com>
2023-03-01 19:56:40 -0700

    add style to like button, like counter

--- src/components/timelineComponents/LikeButton.tsx
+++ src/components/timelineComponents/LikeButton.tsx
@@ -2,12 +2,13 @@ import { BsFillHeartFill } from "react-icons/bs";
 
 export function LikeButton() {
   return (
-    <button className={" hover:scale-110"}>
+    <button className={" flex  items-center hover:scale-110"}>
       <BsFillHeartFill
         color="#8f181e"
         size="1.5rem"
         onClick={() => console.log("post liked")}
       />
+      <span className={" m-1   text-sm text-gray-900"}>{10}</span>
     </button>
   );
 }
